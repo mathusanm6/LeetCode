eacc8ed2f2eee41a976549a587883a6f42a5b48c
mathusanMe <mathusan.selvakumar+github@gmail.com>
2023-11-25 15:44:57 +0100

    Add number of islands

---
+++ Python/test_number_of_islands.py
@@ -0,0 +1,23 @@
+from number_of_islands import Solution
+
+
+def test_num_islands():
+    solution = Solution()
+
+    # Testcase 1
+    grid = [
+        ["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"],
+    ]
+    assert solution.numIslands(grid) == 1
+
+    # Testcase 2
+    grid = [
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"],
+    ]
+    assert solution.numIslands(grid) == 3
