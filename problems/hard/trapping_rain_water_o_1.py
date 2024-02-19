35ae6494420383ae5fcc36c1ca56a4a8540d8c2f
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-02-19 18:55:52 +0100

    feat: Add solution 42. Trapping Rain Water O(1)

---
+++ problems/hard/trapping_rain_water_o_1.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+
+        leftMax = height[0]     # O(1) space
+        rightMax = height[n - 1]
+
+        left = 1
+        right = n - 2
+
+        trapped_water = 0
+
+        while left <= right:
+            if leftMax < rightMax:
+                if height[left] > leftMax:
+                    leftMax = height[left]
+                else:
+                    trapped_water += leftMax - height[left]
+                left += 1
+            else:
+                if height[right] > rightMax:
+                    rightMax = height[right]
+                else:
+                    trapped_water += rightMax - height[right]
+                right -= 1
+
+        return trapped_water
