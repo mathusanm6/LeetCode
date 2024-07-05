a27517bcfd00d751911030743c3f409267614809
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-07-05 15:20:39 +0200

    fix: Solution for Valid Parenthese

--- problems/easy/valid_parentheses.py
+++ problems/easy/valid_parentheses.py
@@ -1,31 +1,24 @@
-from collections import deque
-
-
 class Solution(object):
-    def isOpenBracket(self, c):
-        open_ = "([{"
-        return c in open_
+    def isValid(self, s: str) -> bool:
+        def isOpenBracket(c):
+            open_ = "([{"
+            return c in open_
 
-    def correspondingBracket(self, c):
-        close = ")]}"
-        open_ = "([{"
-        for i in range(len(close)):
-            if close[i] == c:
-                return open_[i]
-        return None
-
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        dq = deque()
+        def correspondingBracket(c):
+            close = ")]}"
+            open_ = "([{"
+            for i in range(len(close)):
+                if close[i] == c:
+                    return open_[i]
+            return None
+    
+        stack = []
         for c in s:
-            if self.isOpenBracket(self, c):
-                dq.append(c)
+            if isOpenBracket(c):
+                stack.append(c)
             else:
-                if len(dq) > 0 and self.correspondingBracket(self, c) == dq[-1]:
-                    dq.pop()
+                if len(stack) > 0 and correspondingBracket(c) == stack[-1]:
+                    stack.pop()
                 else:
                     return False
-        return len(dq) == 0
+        return len(stack) == 0
