889ba5730cf07197b3f129d84726a74349cde2d7
mathusanm6 <hi@mathusan.me>
2025-08-26 22:00:08 +0200

    fix: non standard syntax (C++20)

--- problems/two_sum/two_sum.cc
+++ problems/two_sum/two_sum.cc
@@ -3,21 +3,18 @@
 #include <unordered_map>
 #include <vector>
 
-#include "../common/enumerate.h"
-
 using std::unordered_map;
 using std::vector;
-using std::views::enumerate;
 
 vector<int> twoSum(const vector<int> &nums, const int target)
 {
+    const int n = static_cast<int>(nums.size());
     unordered_map<int, int> index_of;
     index_of.reserve(nums.size()); // Reserve space to avoid rehashing
-
-    for (auto const [idx, num] : enumerate(nums))
+    for (int idx = 0; idx < n; ++idx)
     {
+        const int num = nums[idx];
         int need = target - num;
-
         if (auto it = index_of.find(need); it != index_of.end())
         {
             return {it->second, idx};
